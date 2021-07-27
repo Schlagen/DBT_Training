72dc489e9744b96065e0c88f5044e267b761b46b
Manveer Singh <manveerparmar13@gmail.com>
2021-07-27 10:49:16 +0000

    breaking down dim_customers into stg_customers, stg_orders and dim_customers

--- models/dim_customers.sql
+++ models/dim_customers.sql
@@ -1,25 +1,15 @@
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from DBT_TRAINING_DB.jaffle_shop.customers
+    select * from {{ ref('stg_customers')}}
 
 ),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from DBT_TRAINING_DB.jaffle_shop.orders
+    select * from {{ ref('stg_orders') }}
 
 ),
+
 
 customer_orders as (
 
